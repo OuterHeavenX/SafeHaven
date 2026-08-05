28701406d7f2db25663ba9343a0b8e6ab3d1b56d
OuterHeavenX <60826969+OuterHeavenX@users.noreply.github.com>
2026-08-05 15:17:47 -0500

    Fix SafeHaven Vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
-import type { ConfigEnv } from 'vite';
 
-export default defineConfig(({ command }: ConfigEnv) => ({
-  base: command === 'build' ? '/SafeHaven/' : '/',
+export default defineConfig({
+  base: '/SafeHaven/',
   build: {
     target: 'es2022',
     sourcemap: true,
   },
-}));
+});
